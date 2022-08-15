80fc88c99065bb24d58c4d7aa9cc6f7cf6a04654
ipuppet <zimingw.ol@outlook.com>
2022-08-16 02:30:45 +0800

    update

--- Demo/scripts/libs/easy-jsbox.js
+++ Demo/scripts/libs/easy-jsbox.js
@@ -2854,31 +2854,37 @@ class Setting extends Controller {
 
     get footer() {
         if (this.#footer === undefined) {
-            const info = FileStorage.readFromRootAsJSON("/config.json", {})["info"]
-            this.#footer = info
-                ? {
-                      type: "view",
-                      props: { height: 130 },
-                      views: [
-                          {
-                              type: "label",
-                              props: {
-                                  font: $font(14),
-                                  text: `${$l10n("VERSION")} ${info.version} ♥ ${info.author}`,
-                                  textColor: $color({
-                                      light: "#C0C0C0",
-                                      dark: "#545454"
-                                  }),
-                                  align: $align.center
-                              },
-                              layout: make => {
-                                  make.left.right.inset(0)
-                                  make.top.inset(10)
+            let info = FileStorage.readFromRootAsJSON("config.json", {})["info"] ?? {}
+            if (!info.version || !info.author) {
+                try {
+                    info = __INFO__
+                } catch {}
+            }
+            this.#footer =
+                info.version && info.author
+                    ? {
+                          type: "view",
+                          props: { height: 130 },
+                          views: [
+                              {
+                                  type: "label",
+                                  props: {
+                                      font: $font(14),
+                                      text: `${$l10n("VERSION")} ${info.version} ♥ ${info.author}`,
+                                      textColor: $color({
+                                          light: "#C0C0C0",
+                                          dark: "#545454"
+                                      }),
+                                      align: $align.center
+                                  },
+                                  layout: make => {
+                                      make.left.right.inset(0)
+                                      make.top.inset(10)
+                                  }
                               }
-                          }
-                      ]
-                  }
-                : {}
+                          ]
+                      }
+                    : {}
         }
         return this.#footer
     }
