afd7102a2b2ba32f759e2248390aa81a68160c3a
ipuppet <zimingw.ol@outlook.com>
2022-09-05 23:11:14 +0800

    update

--- src/tasks.js
+++ src/tasks.js
@@ -1,20 +1,16 @@
 class Tasks {
-    #syncDelay = 0
     #tasks = {}
 
     /**
      *
      * @param {Function} task
      * @param {number} delay 单位 s
-     * @param {boolean} useSyncDelay 开启后相对上一个任务的延时添加延时 如 a,b 两个任务延时都为 0.1s, 则开启 SyncDelay 后 b 任务实际将会在 0.2s 后执行
      * @returns
      */
-    addTask(task, delay = 0, useSyncDelay = false) {
+    addTask(task, delay = 0) {
         const uuid = $text.uuid
-        this.#syncDelay += delay
-        this.#tasks[uuid] = $delay(useSyncDelay ? this.#syncDelay : delay, async () => {
+        this.#tasks[uuid] = $delay(delay, async () => {
             await task()
-            this.#syncDelay -= delay
             delete this.#tasks[uuid]
         })
         return uuid
