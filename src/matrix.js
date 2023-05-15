dddd88eb90a7a2500648b8688b923efa54cc3df3
ipuppet <zimingw.ol@outlook.com>
2023-05-15 14:48:44 +0800

    修复 Matrix 长按菜单圆角消失的bug

--- src/matrix.js
+++ src/matrix.js
@@ -131,7 +131,6 @@ class Matrix extends View {
                 hidden: false
             })
         }
-        this.props.template.props = {}
 
         // rebuild template
         const templateViews = [
