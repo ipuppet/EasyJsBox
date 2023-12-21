3d0a160c154d54339d92bffa89ca6e09e51a368d
Samuel <zimingw.ol@outlook.com>
2023-12-21 11:43:36 +0800

    优化输入类型UI

--- src/setting/setting-items.js
+++ src/setting/setting-items.js
@@ -846,9 +846,9 @@ class SettingInput extends SettingItem {
                         // 与标题间距 SettingItem.edgeOffset
                         make.left.equalTo(view.prev.get("label").right).offset(SettingItem.edgeOffset)
                         make.right.inset(SettingItem.edgeOffset)
-                        make.width.greaterThanOrEqualTo(30) // 30 大约是清空按钮的宽度
+                        make.width.greaterThanOrEqualTo(50) // 30 大约是清空按钮的宽度
                         make.height.equalTo(view.super)
-                        make.left.priority(1)
+                        make.left.priority(10)
                         make.width.priority(10)
                     },
                     events: {
